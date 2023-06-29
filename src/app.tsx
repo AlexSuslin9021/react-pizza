501a3f37c988c706ed68cbf3c30ac5b711962497
AlexSuslin9021 <alexsuslin@inbox.ru>
2023-06-29 21:49:54 +0300

    create style

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './app.module.scss';
+
+function App() {
+    return (
+        <div className={s.app}>
+            <div className={s.container}>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
