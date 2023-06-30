f2e8fec1402241511c68f90b3c9aef0bfb886cb5
AlexSuslin9021 <alexsuslin@inbox.ru>
2023-06-30 22:54:03 +0300

    change style app

--- src/Menu/menu.tsx
+++ src/Menu/menu.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Item} from "./common/items";
+import s from './style.module.scss'
 
 export const Menu = () => {
     return (
 
-            <>
+            <div className={s.menuContainer}   >
             <Item/>
             <Item/>
             <Item/>
@@ -15,7 +16,7 @@ export const Menu = () => {
             <Item/>
 
 
-        </>
+        </div>
 
     );
 };
