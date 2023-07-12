ee400041e615de0b81a764d8d684aa8613e0330f
AlexSuslin9021 <alexsuslin@inbox.ru>
2023-07-12 09:49:30 +0300

    add click on body, for close modal

--- src/search/component/sortPizza.tsx
+++ src/search/component/sortPizza.tsx
@@ -1,19 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import s from "../sortFilter.module.scss";
 import {Modal} from "../../common/component/modal/modal";
 
 const SortPizza: React.FC<SortPizzaType> = ({category, setCategory}) => {
     const [open, setOpen] = useState(false)
-
+    const modalRef=useRef<HTMLDivElement>(null)
+    useEffect(()=>{
+        const handleClick=(event:any)=>{
+            if (modalRef.current && !modalRef.current.contains(event.target)){
+                setOpen(false)
+            }
+        }
+        document.body.addEventListener('click', handleClick)
+        return ()=>{
+            document.body.removeEventListener('click', handleClick)
+        }
+    },[])
     const onClickHandler = () => { setOpen(!open) }
     const onClickChangeSort = (value: any) => {
         setCategory(value)
         setOpen(false)
     }
     return (
-        <div className={s.sort}>
+        <div ref={modalRef}  className={s.sort}>
             <span>Сортировать по: <span onClick={onClickHandler} className={s.changeSort}>{category.name}</span></span>
-            {open && <Modal category={category} callback={onClickChangeSort}/>}
+            {open && <Modal category={category} callback={onClickChangeSort}/> }
         </div>
     );
 };
